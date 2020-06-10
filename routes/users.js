4e37b9389f21c8f769b8ce97d289916419fd2efc
Bill Newman <william.newman@seattlecolleges.edu>
2020-06-10 10:45:42 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/test.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  var title = "Our Users Page";
+  res.render('users/index',{
+    title:title,
+    users:data
+  });
 });
 
 module.exports = router;
