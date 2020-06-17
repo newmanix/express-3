12d11618956cb62a907d9a2217a837e6d1f2e2f5
Bill Newman <william.newman@seattlecolleges.edu>
2020-06-17 09:36:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,18 +28,18 @@ collection.find({}, function(err, docs){
     }
 });
 
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
 app.get('/collections/:name',function(req,res){
   var collection = db.get(req.params.name);
   collection.find({},{limit:20},function(e,docs){
     res.json(docs);
   })
 });
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
